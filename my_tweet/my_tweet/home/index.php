4d2b107c95fecf968c0b71f7ef48eae071d45dac
ウパダヤ  アディト <157487302+Adit-Upadhya@users.noreply.github.com>
2025-04-21 09:52:53 +0900

    Add files via upload

---
+++ my_tweet/my_tweet/home/index.php
@@ -0,0 +1,54 @@
+<?php
+// 共通ファイル app.php を読み込み
+require_once('../app.php');
+
+// TODO: ユーザセッションの確認し、ログインしていない場合はログイン画面にリダイレクト
+if (!isset($_SESSION[APP_KEY]['auth_user'])) {
+    header('Location: ../login/');
+    exit;
+}
+
+// TODO: セッションからユーザ情報を取得
+$user = [];
+
+// ユーザセッションの取得
+// TODO: Tweet投稿一覧を取得
+// ダミーデーター
+$tweets = range(1, 5);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<!-- TODO: コンポーネント: components/head.php -->
+<?php include COMPONENT_DIR . 'head.php' ?>
+<body>
+
+    <div class="flex">
+
+        <header class="w-1/5 p-3 border-r min-h-screen">
+            <!-- TODO: components/nav.php 読み込み -->
+            
+            <?php include COMPONENT_DIR . 'nav.php' ?>
+        </header>
+
+        <main class="w-4/5 pt-3">
+            <div class="row">
+                <!-- TODO: components/tweet_form.php 読み込み -->
+                <?php include COMPONENT_DIR . 'tweet_form.php' ?>
+            </div>
+
+            <? if ($tweets) : ?>
+                <?php foreach ($tweets as $value): ?>
+                    <div class="row">
+                        <!-- TODO: components/tweet.php 読み込み -->
+                        <?php include COMPONENT_DIR . 'tweet.php' ?>
+                    </div>
+                <?php endforeach ?>
+            <? endif ?>
+        </main>
+    </div>
+
+</body>
+
+</html>
---
+++ my_tweet/my_tweet/login/auth.php
@@ -0,0 +1,38 @@
+<?php
+// 共通ファイル app.php を読み込み
+require_once '../app.php';
+
+// Userモデルをインポート
+use App\Models\User;
+
+// POSTリクエストでなければ何も表示しない
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    exit;
+}
+
+// TODO: セッションにPOSTデータを登録
+$_SESSION[APP_KEY]['login'] = $_POST;
+
+// 入力されたアカウント名とパスワードを取得
+$account_name = $_POST['account_name'];
+$password = $_POST['password'];
+
+// TODO: ユーザ認証: new User() で auth() を実行
+$auth_user = [];
+if ($account_name === 'test') {
+    $auth_user['id'] = 1; 
+} else {
+    $auth_user['id'] = null; 
+}
+
+if (empty($auth_user['id'])) {
+    // ログイン失敗時はログイン入力画面にリダイレクト
+    header('Location: input.php');
+    exit;
+} else {
+    // TODO: 認証成功時はセッションにユーザデータを保存
+    $_SESSION[APP_KEY]['auth_user'] = $auth_user;
+    // TODO: トップページにリダイレクト
+    header('Location: ../home/');   
+    exit;
+}
